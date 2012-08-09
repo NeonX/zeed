628c1e9f038f0adf39ea3efbdbf49cdd101ab1ad
Narong Rammanee <ranarong@live.com>
2012-08-08 19:07:59 -0700

    Udate Class

--- classes/ComModel.class.php
+++ classes/ComModel.class.php
@@ -1,24 +1,22 @@
 <?php
-
 require_once dirname(__FILE__) . '/DBConnection.class.php';
-
-class ComModel extends DBConnection {
-
+ 
+class ComModel extends DBConnection
+{
     public $post;
     public $prefix = 'tbm_';
-    public $colId = '_id';
-
+    public $colId  = '_id';
     /**
      * 
      * Constructor
      */
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
-    }
+    } // End constructor
 
-// End constructor
-
-    public function getAll($table, $columns, $page, $limit, $sidx, $sord) {
+    public function getAll($table, $columns, $page, $limit, $sidx, $sord)
+    {
         $result = new stdClass();
 
         $sql = "SELECT *, '' AS action FROM {$this->prefix}{$table} WHERE 1=1";
@@ -37,8 +35,8 @@ class ComModel extends DBConnection {
 
         $start = $limit * $page - $limit; // do not put $limit*($page - 1)
 
-        if ($sidx) {
-            $sql .= " ORDER BY " . $sidx;
+        if($sidx) {
+            $sql .= " ORDER BY ".$sidx;
             $sql .= strtoupper($sord) == 'ASC' ? ' ASC' : ' DESC';
         }
 
@@ -51,23 +49,23 @@ class ComModel extends DBConnection {
 
         $stmt->execute();
 
-        $result->page = $page;
-        $result->total = $total_pages;
+        $result->page    = $page;
+        $result->total   =  $total_pages;
         $result->records = $count;
 
         $i = 0;
         $data = array();
         $columns = explode(',', $columns);
 
-        while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+        while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
             foreach ($columns as $key => $value) {
                 if ($value != 'action' && $value != 'deleteflag') {
                     $data[$key] = $row->$value;
                 } else if ($value == 'deleteflag') {
                     $data[$key] = $row->$value ? '<span class="red">Unuse</span>' : '<span class="green">Used</span>';
                 } else {
-                    $data[$key] = self::setActionIcon('view', '../page/images/icons/form-16x16.png', $table, $row->$columns[0]) .
-                            self::setActionIcon('edit', '../page/images/icons/form-edit-16x16.png', $table, $row->$columns[0]);
+                    $data[$key] = self::setActionIcon('view', '../page/images/icons/form-16x16.png', $table, $row->$columns[0]) . 
+                        self::setActionIcon('edit', '../page/images/icons/form-edit-16x16.png', $table, $row->$columns[0]);
                 }
             }
 
@@ -82,8 +80,9 @@ class ComModel extends DBConnection {
         return json_encode($result);
     }
 
-    public function setActionIcon($mode, $src, $cls, $id) {
-        $anchor = '<a href="#customForm" class="' . $cls . '">';
+    public function setActionIcon($mode, $src, $cls, $id)
+    {
+        $anchor  = '<a href="#customForm" class="' . $cls . '">';
         $anchor .= '<img type="image" class="list-icon-view" src="' . $src . '"';
         $anchor .= ' title="' . $mode . '" alt="view" value="' . $id . '"">';
         $anchor .= '</a>';
@@ -91,11 +90,12 @@ class ComModel extends DBConnection {
         return $anchor;
     }
 
-    public function getDataById($table, $id) {
+    public function getDataById($table, $id)
+    {
         $result = new stdClass();
 
-        $id = (int) $id;
-        $colId = $table . $this->colId;
+        $id     = (int) $id;
+        $colId  = $table . $this->colId;
         $fTable = $this->prefix . $table;
 
         $sql = "SELECT * FROM {$fTable} WHERE {$colId} = :id";
@@ -112,39 +112,40 @@ class ComModel extends DBConnection {
             case 'goodsprice':
                 $fk = array(
                     'goodstype_id' => array('goodstype_id', 'goodstype_eng'),
-                    'unit_id' => array('unit_id', 'unitcode'),
-                    'currency_id' => array('currency_id', 'currcode')
+                    'unit_id'      => array('unit_id', 'unitcode'),
+                    'currency_id'  => array('currency_id', 'currcode')
                 );
 
                 $result->goodstype = self::getChildAllById('goodstype', $fk['goodstype_id'], $this->id);
                 $result->unit = self::getChildAllById('unit', $fk['unit_id'], $id);
                 $result->currency = self::getChildAllById('currency', $fk['currency_id'], $this->id);
-                break;
+            break;
             case 'goods':
                 $fk = array(
                     'goodstype_id' => array('goodstype_id', 'goodstype_eng')
                 );
 
                 $result->goodstype = self::getChildAllById('goodstype', $fk['goodstype_id'], $this->id);
-                break;
+            break;
         }
 
         $result->params = array(
             'table' => $table,
-            $colId => $this->id
+            $colId  => $this->id
         );
         $result->stmt = $stmt;
 
         return json_encode($result);
     }
 
-    public function getChildAllById($table, $columns, $id) {
+    public function getChildAllById ($table, $columns, $id)
+    {
         $result = new stdClass();
 
         $fTable = $this->prefix . $table;
 
         $sql = "SELECT {$columns[0]}, {$columns[1]} FROM {$fTable} WHERE 1=1";
-//         var_dump($sql);exit;
+
         $stmt = $this->db->prepare($sql);
 
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
@@ -156,51 +157,36 @@ class ComModel extends DBConnection {
         return $result;
     }
 
-    public function save($post) {
+    public function save($post)
+    {
         $result = new stdClass();
-        $mode = isset($post['mode']) ? $post['mode'] : null;
-        $table = isset($post['table']) ? $post['table'] : null;
-        $colId = $table . $this->colId;
-        $id = isset($post[$colId]) ? (int) $post[$colId] : null;
+        $mode   = isset($post['mode']) ? $post['mode'] : null;
+        $table  = isset($post['table']) ? $post['table'] : null;
+        $colId  = $table . $this->colId;
+        $id     = isset($post[$colId]) ? (int) $post[$colId] : null;
         $fTable = $this->prefix . $table;
 
         $postKey = array_keys($post);
 
-        foreach (range(0, 2) as $val) {
+        foreach (range(0,2) as $val) {
             array_pop($postKey);
         }
 
-        $params = array();
-        switch ($table) {
-            case 'unit':
-            case 'currency':
-            case 'goodstype':
-            case 'goods':
-            case 'goodsprice':
-                if ($mode == 'insert') {
-                    $addCols = array('create_by', 'create_date');
-                    $colArr = array_merge($postKey, $addCols);
-                    $columns = join(',', $colArr);
+        $paramsArr = array();
+        if ($mode == 'insert') {
+            $columns = join(',', $postKey);
+            $colLength = count($postKey);
+            foreach($postKey as $key => $value) {
+                $paramsArr[] = '?';
+            }
 
-                    $colLength = count($colArr);
-                    foreach ($colArr as $key => $value) {
-                        $paramArr[] = '?';
-                    }
-
-                    $params = join(',', $paramArr);
-                } else if ($mode == 'update') {
-                    $addCols = array('lastupdate_by', 'lastupdate_date');
-                    $colArr = array_merge($postKey, $addCols);
-                    $columns = join(',', $colArr);
-
-                    $colLength = count($colArr);
-                    foreach ($colArr as $key => $value) {
-                        $paramArr[] = $value . ' = ' . '?';
-                    }
-
-                    $params = join(',', $paramArr);
-                }
-                break;
+            $params = join(',', $paramsArr);
+        } else if ($mode == 'update') {
+            $colLength = count($postKey);
+            foreach($postKey as $key => $value) {
+                $paramsArr[] = $value . ' = ' . '?';
+            }
+            $params = join(',', $paramsArr);
         }
 
         $sql = array(
@@ -210,30 +196,34 @@ class ComModel extends DBConnection {
         );
 
         $stmt = $this->db->prepare($sql[$mode]);
-        $tmp = array();
+        $myparams = array();
         switch ($mode) {
             case 'insert':
             case 'update':
                 if ($mode == 'insert') {
-                    foreach ($colArr as $key => $value) {
+                    foreach ($postKey as $key => $value) {
                         $index = $key + 1;
                         $stmt->bindValue($index, isset($post[$value]) ? trim($post[$value]) : null);
-
-                        $tmp[$key] = $post[$value];
+                        $myparams['insert'][] =  trim($post[$value]);
                     }
-
-                    $stmt->bindValue($index, isset($post[$value]) ? trim($post[$value]) : null);
                 } else {
-                    foreach ($colArr as $key => $value) {
+                    foreach ($postKey as $key => $value) {
                         $index = $key + 1;
-                        $stmt->bindValue($index, isset($post[$value]) ? trim($post[$value]) : null);
+                        if ($value == 'lastupdate_by') {
+                            $stmt->bindValue($index, isset($post[$value]) ? (int) $post[$value] : null, PDO::PARAM_INT);
+                            $myparams['update'][] = (int) $post[$value];
+                            continue;
+                        } else {
+                            $stmt->bindValue($index, isset($post[$value]) ? trim($post[$value]) : null);
+                            $myparams['update'][] = $post[$value];
+                        }
                     }
-
+                    $myparams['update'][$colLength + 1] = $id;
                     $stmt->bindValue($colLength + 1, $id, PDO::PARAM_INT);
                 }
 
                 $stmt->execute();
-                break;
+            break;
 
             case 'delete':
                 $stmt->bindValue(1, $post['deleteflag'] ? 1 : 0);
@@ -242,13 +232,14 @@ class ComModel extends DBConnection {
 
                 $result->id = $id;
                 $result->deleteflag = $post['deleteflag'] ? 1 : 0;
-                break;
+            break;
 
             default:
                 $result->error_msg = 'CASE NOT FOUND!!!';
                 $result->success = true;
         }
-        $result->tmp = $tmp;
+
+        $result->myparams = $myparams;
         $result->stmt = $stmt;
         $result->mode = $mode;
         $result->row_count = $stmt->rowCount();
@@ -261,8 +252,8 @@ class ComModel extends DBConnection {
      * 
      * @return No value is returned.
      */
-    public function __destruct() {
+    public function __destruct() 
+    {
         parent::__destruct();
     }
-
 }
